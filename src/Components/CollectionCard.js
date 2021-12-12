64244abf8f6a252028d12f1d3a4cc2b08f3918a0
Vikash <21vikashgupta@gmail.com>
2021-12-13 00:02:59 +0530

    Header completed

---
+++ src/Components/CollectionCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CollectionCard = () => {
+  return (
+    <div className="collectionCard">
+      <img
+        src="https://nftlabs.mypinata.cloud/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default CollectionCard;
